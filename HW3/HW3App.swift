0fbae1f9c1cab1487e0f9879944323d6dd445dbb
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2023-09-14 12:38:44 -0600

    Initial Commit

---
+++ HW3/HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3App.swift
+//  HW3
+//
+//  Created by octopus on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
